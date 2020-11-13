fe6108b04c76ff1b8fd11c8bcb60d10bd5b35d7a
Marcos Dacosta <uchodc@gmail.com>
2020-11-13 13:58:19 +0000

    Section WIP

--- src/classes/environment/section/section.ts
+++ src/classes/environment/section/section.ts
@@ -1,9 +1,27 @@
+import * as THREE from 'three'
+
+export type Position = {
+    x: number
+    y: number
+    z: number
+}
+
+class SectionService {
+    generate(scale: number, walls: Array<Boolean>) {
+        let geometry = new THREE.BoxGeometry(scale, scale, scale)
 
 
-class Section {
-    type: string
+    }
+}
 
-    constructor(type: string) {
+export class Section {
+    private type: string
+    private scale: number
+    walls: Array<Boolean>
+
+    constructor(type: string, scale: number, walls: Array<Boolean>, position: Position) {
         this.type = type
+        this.scale = scale
+        this.walls = walls
     }
 }
